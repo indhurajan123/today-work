7becd468de52a9345e9f25d364a9e022a5aac5e4
indhurajan123 <indhulekha1515@gmail.com>
2024-11-28 12:28:16 +0530

    mozilla

---
+++ Mozilla.java
@@ -0,0 +1,14 @@
+package browser_launching;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Mozilla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new FirefoxDriver();
+
+	}
+
+}
